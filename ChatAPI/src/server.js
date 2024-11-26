1cbc56c1cd3422f963ee18083822ed9c79f7cb18
JanisEnzler <52508299+JanisEnzler@users.noreply.github.com>
2024-11-26 17:38:55 +0100

    Signup, Login, Token creation and validation

--- ChatAPI/src/server.js
+++ ChatAPI/src/server.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+const crypto = require('crypto');
+
 const dbPassword = process.env.DB_PASSWORD || 'password';
 const dbUser = process.env.DB_USER || 'user';
 const dbHost = process.env.DB_HOST || 'localhost';
 const dbPort = process.env.DB_PORT || 5432;
 const dbName = process.env.DB|| 'postgres';
+const adminPassword = process.env.ADMIN_PASSWORD || 'admin';
+const adminSecret = process.env.ADMIN_SECRET || 'defaultSecret';
 
 const { Pool } = require('pg');
 
@@ -46,6 +50,86 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.get('/signup', async (req, res) => {
+  const userName = req.query?.username?.toString();
+  const userPassword = req.query?.password?.toString();
+
+  if (!userName || !userPassword) {
+    res.status(400).send('Username or password is missing.');
+    return;
+  }
+
+
+  // Check username validity
+  if (!/^[a-zA-Z0-9_]+$/.test(userName)) {
+    res.status(400).send('Username contains invalid characters.');
+    return;
+  }
+
+  // Username should be between 3 and 20 characters long
+  if (userName.length < 3 || userName.length > 20) {
+    res.status(400).send('Username should be between 3 and 20 characters long.');
+    return;
+  }
+
+  // Check password validity
+  if (!/^[a-zA-Z0-9_]+$/.test(userPassword)) {
+    res.status(400).send('Password contains invalid characters.');
+    return;
+  }
+
+  // Password should be between 8 and 32 characters long
+  if (userPassword.length < 8 || userPassword.length > 32) {
+    res.status(400).send('Password should be between 8 and 32 characters long.');
+    return;
+  }
+
+  // Check if the username is already in the DB, and if not, add it (this should be done in a transaction)
+
+  const result = await pool.query('SELECT * FROM users WHERE username = $1', [userName]);
+  if (result.rows.length > 0) {
+    res.status(400).send('Username already exists.');
+    return;
+  }else{
+    try {
+      const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [userName, userPassword]);
+      res.status(201).json(result.rows[0].userID);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+
+
+
+});
+
+// Takes username and password and returns a token
+app.get('/login', async (req, res) => {
+  const userName = req.query?.username?.toString();
+  const userPassword = req.query?.password?.toString();
+
+  // Check if the username and password are provided
+  if (!userName || !userPassword) {
+    res.status(400).send('Username or password is missing.');
+    return;
+  }
+
+  // Retrieve the user from the database
+  const result = await pool.query('SELECT * FROM users WHERE username = $1', [userName]);
+  if (result.rows.length === 0) {
+    res.status(404).send('User not found.');
+    return;
+  }
+
+  if (result.rows[0].password !== userPassword) {
+    res.status(401).send('Incorrect password.');
+    return;
+  }else{
+    const token = createToken(result.rows[0].id, userPassword);
+    res.status(200).json({ token: token });
+  }
+});
 
 // history
 app.get('/history', async (req, res) => {
@@ -172,3 +256,26 @@ app.delete('/nicknames/:id', async (req, res) => {
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+
+
+function createToken(userID, userPassword) {
+  // Expiration date = curent date + 7 days
+  expirationDate = date.setDate(date.getDate() + 7);
+
+  // Create a session token by hashing the user ID, User Password, adminSecret, and the expiration date
+  return crypto.createHmac('sha256', adminSecret)
+    .update(userID + adminPassword + expirationDate)
+    .digest('hex'), expirationDate;
+}
+
+function verifyToken(token, userID, expirationDate) {
+  // retrieve user password from database
+  userPassword = pool.query('SELECT password FROM users WHERE id = $1', [userID]);
+
+  // Verify the token by hashing the user ID, User Password, adminSecret, and the expiration date
+  return token === crypto.createHmac('sha256', adminSecret)
+    .update(userID + adminPassword + expirationDate)
+    .digest('hex');
+}
