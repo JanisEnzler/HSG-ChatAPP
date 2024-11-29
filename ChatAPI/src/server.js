2728202bd49b6c6ae7986446a0af815d850acaa4
janis <janis.enzler@outlook.de>
2024-11-29 23:00:18 +0100

    Added Requests & Fixes

--- ChatAPI/src/server.js
+++ ChatAPI/src/server.js
@@ -6,6 +6,8 @@ var port = 3000;
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
+const { validate: isUuid } = require('uuid');
+
 
 const dbPassword = process.env.DB_PASSWORD || 'password';
 const dbUser = process.env.DB_USER || 'admin';
@@ -54,6 +56,7 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Create a new user
 app.post('/signup', async (req, res) => {
   const userName = req.body?.username?.toString();
   const userPassword = req.body?.password?.toString();
@@ -105,6 +108,7 @@ app.post('/signup', async (req, res) => {
     }
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 });
 
@@ -137,13 +141,15 @@ app.get('/login', async (req, res) => {
     }
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 });
 
+// Validate the token
 app.get('/validate', async (req, res) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];  
-  
+  const token = authHeader && authHeader.split(' ')[1];
+
   if (!verifyToken(token)) {
     res.status(401).send('Invalid or expired token.');
     return;
@@ -160,6 +166,7 @@ app.get('/history', async (req, res) => {
     res.status(200).json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 
 });
@@ -187,6 +194,7 @@ app.post('/history', async (req, res) => {
     }
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 
   // Get the current, local time
@@ -198,10 +206,10 @@ app.post('/history', async (req, res) => {
     res.status(201).json(result.rows[0]);
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 });
 
-// nicknames
 app.get('/nicknames', async (req, res) => {
   // retrieve all nicknames from the database
   try {
@@ -209,9 +217,9 @@ app.get('/nicknames', async (req, res) => {
     res.status(200).json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 });
-
 
 app.get('/nicknames/:id', async (req, res) => {
   const id = +req.params.id;
@@ -225,6 +233,7 @@ app.get('/nicknames/:id', async (req, res) => {
     res.json(result.rows[0]);
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 });
 
@@ -259,6 +268,7 @@ app.post('/nicknames', async (req, res) => {
     res.status(201).json(result.rows[0]);
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 });
 
@@ -274,9 +284,11 @@ app.delete('/nicknames/:id', async (req, res) => {
     res.status(200).json(result.rows[0]);
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 });
 
+// Send a message to a specific group
 app.post('/chats/:id', async (req, res) => {
   const id = req.params.id;
   const chatMessage = req.body?.message?.toString();
@@ -285,16 +297,19 @@ app.post('/chats/:id', async (req, res) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   const userId = jwt.decode(token).id;
-  
+
   if (!verifyToken(token)) {
     res.status(401).send('Access Denied.');
     return;
   }
 
-  
+  // Check if the id is in the correct format
+  if (!isUuid(id)) {
+    res.status(400).send('Invalid id format.');
+    return;
+  }
 
   // Check if the user is part of the group
-
   try {
     const result = await pool.query(
       'SELECT * FROM user_groups WHERE user_id = $1 AND group_id = $2',
@@ -317,10 +332,11 @@ app.post('/chats/:id', async (req, res) => {
 
   } catch (err) {
     res.status(500).json({ error: err.message });
+    console.error(err.message);
   }
 });
 
-// Create a new chat group
+// Create a new group
 app.post('/chats', async (req, res) => {
   // Get list of user ids from the request body
   const userIds = req.body?.userIds;
@@ -351,36 +367,115 @@ app.post('/chats', async (req, res) => {
 
     // Add users to the group
     for (const id of userIds) {
+      // Check if the id is in the correct format
+      if (!isUuid(id)) {
+        res.status(400).send('Invalid id format.');
+        return;
+      }
       await pool.query('INSERT INTO user_groups (user_id, group_id) VALUES ($1, $2)', [id, groupId]);
     }
 
     // Add the creator to the group
     await pool.query('INSERT INTO user_groups (user_id, group_id) VALUES ($1, $2)', [userId, groupId]);
-    
+
     res.status(201).json(groupResult.rows[0]);
 
-  } catch (error) {
+  } catch (err) {
     try {
       await pool.query('DELETE FROM groups WHERE group_name = $1', [groupName]);
     } catch (deleteError) {
       console.error('Error deleting group:', deleteError.message);
     }
 
-    if (error.code === '23505') {
+    if (err.code === '23505') {
       res.status(409).json({ error: 'Duplicate entry. Unique constraint violated.' });
-    } else if (error.code === '23503') {
+    } else if (err.code === '23503') {
       res.status(404).json({ error: 'Foreign key constraint violated.' });
     } else {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ error: err.message });
+      console.error(err.message);
     }
   }
 });
 
+// Get all groups for a specific user
+app.get('/chats', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  const userId = jwt.decode(token).id;
+  if (!verifyToken(token)) {
+    res.status(401).send('Access Denied.');
+    return;
+  }
+
+  try {
+    const result = await pool.query(
+      'SELECT * FROM groups WHERE group_id IN (SELECT group_id FROM user_groups WHERE user_id = $1)',
+      [userId]
+    );
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.error(err.message);
+  }
+});
+
+// Get all messages for a specific group
+app.get('/chats/:id', async (req, res) => {
+  const id = req.params.id;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  const userId = jwt.decode(token).id;
+  if (!verifyToken(token)) {
+    res.status(401).send('Access Denied.');
+    return;
+  }
+
+  // Check if the id is in the correct format
+  if (!isUuid(id)) {
+    res.status(400).send('Invalid id format.');
+    return;
+  }
+
+  try {
+    // Check if the user is part of the group
+    const memberResult = await pool.query(
+      'SELECT * FROM user_groups WHERE user_id = $1 AND group_id = $2',
+      [userId, id]
+    );
+
+    if (memberResult.rows.length === 0) {
+      res.status(403).send('User is not part of the group.');
+      return;
+    }
+
+    const messagesResult = await pool.query(
+      // Join the messages table with the users table, where the group_id is the one provided
+      'SELECT messages.message_id, messages.message, messages.timestamp, users.username FROM messages JOIN users ON messages.user_id = users.user_id WHERE group_id = $1', [id]
+    );
+    res.status(200).json(messagesResult.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.error(err.message);
+  }
+
+});
+
+// Get all users
+app.get('/users', async (req, res) => {
+  try {
+    // retrieve all users from the database and dont send the password
+    const result = await pool.query('SELECT user_id, username FROM users');
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.error(err.message);
+  }
+});
 
 app.listen(app.get('port'), function () {
   console.log('ChatAPI app is running on port', app.get('port'));
 });
-
 
 function generateToken(userID) {
   const payload = { id: userID };
@@ -389,7 +484,6 @@ function generateToken(userID) {
   return token;
 }
 
-// async function to verify the token
 function verifyToken(token) {
   try {
     return jwt.verify(token, adminSecret);
