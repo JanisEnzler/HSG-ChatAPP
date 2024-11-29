55de48b500b0e91a6228cf39e00221e7dff0e2e3
janis <janis.enzler@outlook.de>
2024-11-29 19:21:43 +0100

    New API calls
    API calls, for creating Groups, and writing Group messages

--- ChatAPI/src/server.js
+++ ChatAPI/src/server.js
@@ -5,6 +5,7 @@ var port = 3000;
 
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
 
 const dbPassword = process.env.DB_PASSWORD || 'password';
 const dbUser = process.env.DB_USER || 'admin';
@@ -64,7 +65,7 @@ app.post('/signup', async (req, res) => {
 
 
   // Check username validity
-  if (!/^[a-zA-Z0-9_]+$/.test(userName)) {
+  if (!/^[a-zA-Z0-9_ -]+$/.test(userName)) {
     res.status(400).send('Username contains invalid characters.');
     return;
   }
@@ -76,7 +77,7 @@ app.post('/signup', async (req, res) => {
   }
 
   // Check password validity
-  if (!/^[a-zA-Z0-9_]+$/.test(userPassword)) {
+  if (!/^[a-zA-Z0-9!@#$%^&*]+$/.test(userPassword)) {
     res.status(400).send('Password contains invalid characters.');
     return;
   }
@@ -87,7 +88,7 @@ app.post('/signup', async (req, res) => {
     return;
   }
 
-  // Check if the username is already in the DB, and if not, add it (this should be done in a transaction)
+  // Check if the username is already in the DB, and if not, add it
   try {
     const result = await pool.query('SELECT * FROM users WHERE username = $1', [userName]);
     if (result.rows.length > 0) {
@@ -95,11 +96,12 @@ app.post('/signup', async (req, res) => {
       return;
     } else {
 
-      const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [userName, userPassword]);
-      const token = generateToken(result.rows[0].id);
+      hashedPassword = crypto.createHash('sha256', adminSecret).update(userPassword).digest('hex');
 
-      res.cookie('token', token, { httpOnly: true, secure: true });
-      res.status(201).json({ message: 'Signup successful' });
+      const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [userName, hashedPassword]);
+      const token = generateToken(result.rows[0].user_id);
+
+      res.status(201).json({ token: token });
     }
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -123,18 +125,31 @@ app.get('/login', async (req, res) => {
       return;
     }
 
-    if (result.rows[0].password !== userPassword) {
+    hashedPassword = crypto.createHash('sha256', adminSecret).update(userPassword).digest('hex');
+
+    if (result.rows[0].password !== hashedPassword) {
       res.status(401).send('Incorrect password.');
       return;
     } else {
-      const token = generateToken(result.rows[0].id);
+      const token = generateToken(result.rows[0].user_id);
 
-      res.cookie('token', token, { httpOnly: true, secure: true });
-      res.status(200).json({ message: 'Login successful' });
+      res.status(200).json({ token: token });
     }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+});
+
+app.get('/validate', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];  
+  
+  if (!verifyToken(token)) {
+    res.status(401).send('Invalid or expired token.');
+    return;
+  }
+  user_id = jwt.decode(token).id;
+  res.status(200).send(`Valid token for user: ${user_id}`);
 });
 
 // history
@@ -147,17 +162,6 @@ app.get('/history', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 
-});
-
-app.get('/validate', async (req, res) => {
-  const token = req.cookies.token;
-
-  if (!verifyToken(token)) {
-    res.status(401).send('Invalid or expired token.');
-    return;
-  }
-  user_id = jwt.decode(token).id;
-  res.status(200).send(`Valid token for user: ${user_id}`);
 });
 
 app.post('/history', async (req, res) => {
@@ -175,13 +179,13 @@ app.post('/history', async (req, res) => {
   }
 
   try {
-  // Check if the nickname exists in the DB
-  const result = await pool.query('SELECT * FROM nicknames WHERE id = $1', [nickname_id]);
-  if (result.rows.length === 0) {
-    res.status(400).send('Nickname not found.');
-    return;
-  }
-  } catch (err) { 
+    // Check if the nickname exists in the DB
+    const result = await pool.query('SELECT * FROM nicknames WHERE id = $1', [nickname_id]);
+    if (result.rows.length === 0) {
+      res.status(400).send('Nickname not found.');
+      return;
+    }
+  } catch (err) {
     res.status(500).json({ error: err.message });
   }
 
@@ -245,12 +249,12 @@ app.post('/nicknames', async (req, res) => {
   }
 
   try {
-  /* Check if the nickname is already in the DB */
-  var result = await pool.query('SELECT * FROM nicknames WHERE nickname = $1', [userName]);
-  if (result.rows.length > 0) {
-    res.status(400).send('Nickname already exists.');
-    return;
-  }
+    /* Check if the nickname is already in the DB */
+    var result = await pool.query('SELECT * FROM nicknames WHERE nickname = $1', [userName]);
+    if (result.rows.length > 0) {
+      res.status(400).send('Nickname already exists.');
+      return;
+    }
     result = await pool.query('INSERT INTO nicknames (nickname) VALUES ($1) RETURNING *', [userName]);
     res.status(201).json(result.rows[0]);
   } catch (err) {
@@ -273,11 +277,109 @@ app.delete('/nicknames/:id', async (req, res) => {
   }
 });
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
+app.post('/chats/:id', async (req, res) => {
+  const id = req.params.id;
+  const chatMessage = req.body?.message?.toString();
+  // Get beared token from the request
+
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  const userId = jwt.decode(token).id;
+  
+  if (!verifyToken(token)) {
+    res.status(401).send('Access Denied.');
+    return;
+  }
+
+  
+
+  // Check if the user is part of the group
+
+  try {
+    const result = await pool.query(
+      'SELECT * FROM user_groups WHERE user_id = $1 AND group_id = $2',
+      [userId, id]
+    );
+
+    if (result.rows.length === 0) {
+      res.status(403).send('User is not part of the group.');
+      return;
+    }
+
+    if (!chatMessage) {
+      res.status(400).send('Message is missing.');
+      return;
+    }
+
+    const message_result = await pool.query('INSERT INTO messages (message, group_id, user_id, timestamp ) VALUES ($1, $2, $3, $4) RETURNING *', [chatMessage, id, userId, new Date()]);
+
+    res.status(201).json(message_result.rows[0]);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Create a new chat group
+app.post('/chats', async (req, res) => {
+  // Get list of user ids from the request body
+  const userIds = req.body?.userIds;
+  const groupName = req.body?.groupName?.toString();
+
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  const userId = jwt.decode(token).id;
+  if (!verifyToken(token)) {
+    res.status(401).send('Access Denied.');
+    return;
+  }
+
+  if (!groupName) {
+    res.status(400).send('Group name is missing.');
+    return;
+  }
+
+  if (!userIds || userIds.length < 1) {
+    res.status(400).send('Invalid UserIds.');
+    return;
+  }
+
+  try {
+    // Create a new group
+    const groupResult = await pool.query('INSERT INTO groups (group_name) VALUES ($1) RETURNING *', [groupName]);
+    const groupId = groupResult.rows[0].group_id;
+
+    // Add users to the group
+    for (const id of userIds) {
+      await pool.query('INSERT INTO user_groups (user_id, group_id) VALUES ($1, $2)', [id, groupId]);
+    }
+
+    // Add the creator to the group
+    await pool.query('INSERT INTO user_groups (user_id, group_id) VALUES ($1, $2)', [userId, groupId]);
+    
+    res.status(201).json(groupResult.rows[0]);
+
+  } catch (error) {
+    try {
+      await pool.query('DELETE FROM groups WHERE group_name = $1', [groupName]);
+    } catch (deleteError) {
+      console.error('Error deleting group:', deleteError.message);
+    }
+
+    if (error.code === '23505') {
+      res.status(409).json({ error: 'Duplicate entry. Unique constraint violated.' });
+    } else if (error.code === '23503') {
+      res.status(404).json({ error: 'Foreign key constraint violated.' });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  }
 });
 
 
+app.listen(app.get('port'), function () {
+  console.log('ChatAPI app is running on port', app.get('port'));
+});
 
 
 function generateToken(userID) {
